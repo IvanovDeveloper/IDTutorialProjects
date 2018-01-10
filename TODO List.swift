6e001f1e3df3b74abe805c98246a8994f7a8e60f
Ivanov Developer <ivanov.developer@mail.ru>
2018-01-10 22:24:15 +0200

    added TODO List

---
+++ TODO List.swift
@@ -0,0 +1,13 @@
+//
+//  TODO List.swift
+//  Tutorial-MVC
+//
+//  Created by Andrey Ivanov on 10.01.18.
+//  Copyright © 2018 Ivanov Developer. All rights reserved.
+//
+
+import Foundation
+
+
+// 1. Разобраться с typealias
+typealias JSON = [String : AnyObject]
