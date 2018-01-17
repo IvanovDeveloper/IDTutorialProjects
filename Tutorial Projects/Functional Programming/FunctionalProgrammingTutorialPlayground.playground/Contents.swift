7847436491b5a67d9d910d0e52b6f203ca8aef53
Ivanov Developer <ivanov.developer@mail.ru>
2018-01-17 14:06:37 +0200

    commit

--- Tutorial Projects/Functional Programming/FunctionalProgrammingTutorialPlayground.playground/Contents.swift
+++ Tutorial Projects/Functional Programming/FunctionalProgrammingTutorialPlayground.playground/Contents.swift
@@ -6,13 +6,13 @@ var str = "Hello, playground"
 
 
 // Имеративный способ фильтрации
-var events: [Int] = []
+var evens: [Int] = []
 for i in 1...10 {
     if i % 2 == 0 {
-        events.append(i)
+        evens.append(i)
     }
 }
-print("Imperative method: \(events)")
+print("Imperative method: \(evens)")
 
 
 func isEven(number: Int) -> Bool {
@@ -20,17 +20,17 @@ func isEven(number: Int) -> Bool {
 }
 
 // Functionality method
-events = Array(1...10).filter(isEven)
+evens = Array(1...10).filter(isEven)
 
-print("Functionality method: \(events)")
+print("Functionality method: \(evens)")
 
 // Short Functionality method
-events = Array(1...10).filter { (number) in number % 2 == 0 }
-print("Short Functionality method: \(events)")
+evens = Array(1...10).filter { (number) in number % 2 == 0 }
+print("Short Functionality method: \(evens)")
 
 // Shorter Functionality method
-events = Array(1...10).filter { $0 % 2 == 0 }
-print("Shorter Functionality method: \(events)")
+evens = Array(1...10).filter { $0 % 2 == 0 }
+print("Shorter Functionality method: \(evens)")
 
 // MARK: Filters
 
@@ -44,10 +44,19 @@ func myFilter<T>(source:[T], predicate: (T) -> Bool) -> [T] {
     return result
 }
 
-events = myFilter(source: Array(1...10), predicate: isEven)
-print(events)
+evens = myFilter(source: Array(1...10), predicate: isEven)
+print(evens)
 
-// Reduce filter
+// Filter Reduce
+var evenSum = 0
+for i in evens {
+    evenSum += i
+}
+print("Evens sum = \(evenSum)")
 
 
+evenSum = Array(1...10)
+    .filter { (number) in number % 2 == 0 }
+    .reduce(0) { (total, number) in total + number }
 
+print(evenSum)
