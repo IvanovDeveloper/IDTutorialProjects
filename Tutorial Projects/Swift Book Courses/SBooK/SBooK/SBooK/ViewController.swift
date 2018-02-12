ca49250a55291b1108a06c05265d18a322c30e99
Ivanov Developer <ivanov.developer@mail.ru>
2018-02-12 14:54:13 +0200

    added SBooK project (Swift book example)

---
+++ Tutorial Projects/Swift Book Courses/SBooK/SBooK/SBooK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SBooK
+//
+//  Created by Ivanov Developer on 2/12/18.
+//  Copyright © 2018 Andrii Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
